7be9b93873ac3a397a279671f15a30e5df1503b5
DanieleGsu <danielegsu@gmail.com>
2022-05-17 11:24:28 +0200

    First try

---
+++ boids.hpp
@@ -0,0 +1,18 @@
+#include <cmath>
+#include <iostream>
+#include <vector>
+
+struct dati {
+  std::vector<double> velox{0.,0,};
+  std::vector<double> position{0.,0.};
+  double const ds;
+}
+
+class Boids {
+ public:
+  Boids() : {}
+  std::vector<dati> velox {}
+}
+
+if (d < abs(x_bi - x_bj)) {
+}
