520e4c8eadbb45e8cbb9e73f1db9542224898c3b
Alessandro Coppola <alessandro.coppola5@studio.unibo.it>
2022-05-17 22:20:37 +0200

    Introduced Algorithms

--- boid.hpp
+++ boid.hpp
@@ -1,12 +1,6 @@
 #ifndef BOID_HPP
 #define BOID_HPP
 
-struct boids {
-  double vx{};
-  double vy{};
-  double px{};
-  double py{};
-  double ds{};  // represent the "dimension" (radius) of each boid
-};
+
 
 #endif
--- simulation.hpp
+++ simulation.hpp
@@ -1,10 +1,18 @@
 #ifndef SIMULATION_HPP
 #define SIMULATION_HPP
+#include <algorithm>
+#include <cmath>
+#include <numeric>
 #include <stdexcept>
 #include <vector>
 
-#include "boid.hpp"
-
+struct boids {
+  double vx{};
+  double vy{};
+  double px{};
+  double py{};
+  double const ds{};  // represent the "dimension" (radius) of each boid
+};
 class flock {
   std::vector<boids> boid_;  // vector which represent each boid
   double d_;
@@ -12,24 +20,43 @@ class flock {
  public:
   flock(std::vector<boids> boid, double d) : boid_{boid}, d_{d} {};
 
+  float operator+() {}
+
   int size() const { return boid_.size(); }
 
-  // refit with pointers
   double mass_center() {
     double x_tot = 0;  // sum of x position
     double y_tot = 0;  // sum of y position
     int l = size();
-    for (int i = 1; i <= l; ++i) {
-      x_tot = x_tot + boid_[i].px;
-      y_tot = y_tot + boid_[i].py;
-    }
+    int i = 0;
+
+    // introdurre lambda
+
+    auto x_tot = std::accumulate(boid_.begin(), boid_.end(), 0);
+    auto y_tot = std::accumulate(boid_.begin(), boid_.end(), 0);
+
     auto x_cm = x_tot / (l - 1);
     auto y_cm = y_tot / (l - 1);
-    return x_cm;
-    return y_cm;
   }
+  // Regola 1 : Separazione
+  double separation() {
+    for (int i, j; i != j; ++i, ++j) {
+      double vx_1 = 0;
+      if (std::abs(boid_[i].px - boid_[j].px) < boid_[1].ds) {
+        double const s = 0.5;
+        vx_1 = -s * (boid_[j].px - boid_[i].px);
+      }
+      return vx_1;
+      double vy_1 = 0;
+      if (std::abs(boid_[i].py - boid_[j].py) < boid_[1].ds) {
+        double const s = 0.5;
+        vx_1 = -s * (boid_[j].py - boid_[i].py);
+      }
+      return vy_1;
+    }
+  }
+  // Regola 2: Allineamento
+  // Regola 3 : Coesione
 };
-
-
 
 #endif
