828070e4d8d25025ca6d1a21432470819ac456c1
DanieleGsu <danielegsu@gmail.com>
2022-05-24 13:00:22 +0200

    implementazione coesion

--- simulation.hpp
+++ simulation.hpp
@@ -1,17 +1,20 @@
 #ifndef SIMULATION_HPP
 #define SIMULATION_HPP
+
 #include <cmath>
 #include <stdexcept>
 #include <vector>
+
 constexpr double ds{10.};  // Check if
 constexpr double s = 0.5;
+constexpr double coesion_factor = 1;
 
 struct boid {
   double vx{};
   double vy{};
   double px{};
   double py{};
-  double const ds{};
+  // double const ds{};
 };
 
 class flock {
@@ -21,7 +24,7 @@ class flock {
  public:
   flock(std::vector<boid> flock, double d) : flock_{flock}, d_{d} {}
 
-  int size() const { return flock_.size(); }
+  int size() const { return flock_.size(); }  // ! check it
   boid boid_state(int i) { return flock_[i]; }
 
   // Struct that represent the mass center
@@ -29,6 +32,7 @@ class flock {
     double x_cm{};
     double y_cm{};
   };
+
   CM mass_center() {
     double x_tot = 0;
     double y_tot = 0;
@@ -46,12 +50,14 @@ class flock {
   }
 
   // Rules
+
   //  Rule 1 : Separation
   // struct that represent separation velocity
   struct separation_v {
     double vx_1;
     double vy_1;
   };
+
   separation_v separation() {
     separation_v v1;
     double sum_vx1 = 0;
@@ -76,10 +82,26 @@ class flock {
 
     return v1;
   }
+
   // Rule 2: Aligment
   double aligment() {}
+
   // Rule 3 : Coesion
-  double coesion() {}
+  struct coesion_v {
+    double vx_3;
+    double vy_3;
+  };
+
+  coesion_v coesion() {
+    coesion_v v3;
+    CM p_cm = mass_center();
+    int j;
+
+    v3.vx_3 = coesion_factor * (p_cm.x_cm - flock_[j].px);
+    v3.vy_3 = coesion_factor * (p_cm.y_cm - flock_[j].py);
+
+    return v3;
+  }
 };
 
 #endif
